19a427de127aa6eed92dfee3c8a66c3365273c10
jameslazo <james.lazo+git@protonmail.com>
2025-01-08 06:50:29 -0700

    comments

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,17 +9,3 @@ def test_api_call():
 
 def test_main():
     main()
-
-
-""" TODO
-API Call Tests
-1. assert API_call status code
-2. assert response JSON keys
-
-S3 Bucket Tests
-1. Check for S3_bucket
-2. if not S3_bucket Create S3 bucket 
-3. assert S3_bucket
-4. if not JSON_file put JSON_file object
-5. assert get JSON_file object
-"""
